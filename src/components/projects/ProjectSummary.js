1b56094d3d58223a0b54489bc702d7a0e318011b
Student <example@example.com>
2019-07-23 16:35:11 -0700

    complete lesson 7.  extracting route parameters

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div>
+            <div className='section project-list'>
+                <div className=' card z-depth project-summary'>
+                    <div className='card-content grey-text text-darken-3'>
+                        <span className='card-title'>Project Title</span>
+                        <p>Posted by the Net Ninja</p>
+                        <p className='grey-text'>3rd Spetemner, 2am</p>
+                    </div>
+                </div>
+            </div>           
+        </div>
+    )
+}
+
+export default ProjectSummary
