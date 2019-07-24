5faaef4413c445512cc0f159c3406e4cfb45d394
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-23 21:52:31 -0700

    create store, store file structure and reducers

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <SignedInLinks/>
                 <SignedOutLinks/>
             </div>
-        </nav>      
+        </nav>
     )
 }
 
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,7 +5,7 @@ const SignedInLinks = () => {
     return (
         <ul className='right'>
             <li>
-                <NavLink to='/'>New Projects</NavLink>
+                <NavLink to='/createproject'>New Projects</NavLink>
             </li>
             <li>
                 <NavLink to='/'>Log Out</NavLink>
