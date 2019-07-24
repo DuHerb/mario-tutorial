3e87a680937f83aaf61f40400f2a5be86c8dfbdc
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-23 22:08:44 -0700

    add dummy data and passed it from dashboard into project summary

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div>
             <div className='section project-list'>
                 <div className=' card z-depth project-summary'>
                     <div className='card-content grey-text text-darken-3'>
-                        <span className='card-title'>Project Title</span>
+                        <span className='card-title'>{project.title}</span>
                         <p>Posted by the Net Ninja</p>
                         <p className='grey-text'>3rd Spetemner, 2am</p>
                     </div>
                 </div>
-            </div>           
+            </div>
         </div>
     )
 }
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'help me find peach', content: 'some text goes here for content'},
+    {id: '2', title: 'stomp some goombahs', content: 'some text goes here for content'},
+    {id: '3', title: 'eat some mushrooms', content: 'some text goes here for content'},
+    {id: '4', title: 'gather coins', content: 'some text goes here for content'},
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state;
