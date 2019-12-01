2ca2eaaedf689273c71e5aaf85d4224f88c1c2fa
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-11-30 23:12:15 -0800

    convert signup and signin forms to react hook forms

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,49 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { signIn } from '../../store/actions/authActions'
 import { Redirect } from 'react-router-dom'
+import useForm from "react-hook-form"
 
-class SignIn extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
+const SignIn = ({ signIn, authError, auth }) => {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => signIn(data)
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]:e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.signIn(this.state);
-  }
-  render() {
-    const {authError, auth} = this.props;
-    if(auth.uid) return <Redirect to='/'/>
-    return (
-      <div className='container'>
-        <form onSubmit={this.handleSubmit} className='white'>
-          <h5 className='grey-text text-darken-3'>Sign In</h5>
-          <div className='input-field'>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' onChange={this.handleChange}/>
+  if(auth.uid) return <Redirect to='/'/>
+  return (
+    <div className='container'>
+      <form onSubmit={handleSubmit(onSubmit)} className='white'>
+        <h5 className='grey-text text-darken-3'>Sign In</h5>
+        <div className='input-field'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' name="email" ref={register({required: true})}/>
+        </div>
+        <div className='input-field'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name="password" ref={register({required: true})}/>
+        </div>
+        <div className='input-field'>
+          <button className='btn pink lighten-1 z-depth-0'>Login</button>
+          <div className='red-text center'>
+            { authError ? <p>{authError}</p> : null}
           </div>
-          <div className='input-field'>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' onChange={this.handleChange}/>
-          </div>
-          <div className='input-field'>
-            <button className='btn pink lighten-1 z-depth-0'>Login</button>
-            <div className='red-text center'>
-              { authError ? <p>{authError}</p> : null}
-            </div>
-          </div>
-        </form>
-      </div>
-    )
-  }
+        </div>
+      </form>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
@@ -53,10 +39,10 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDisptachToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
   return {
     signIn: (creds) => dispatch(signIn(creds))
   }
 }
-export default connect(mapStateToProps, mapDisptachToProps)(SignIn)
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
 
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,48 +1,88 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { signUp } from '../../store/actions/authActions'
+import useForm from "react-hook-form"
 
-class SignUp extends Component {
-  state = {
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: ''
-  }
+// class SignUp extends Component {
+//   state = {
+//     email: '',
+//     password: '',
+//     firstName: '',
+//     lastName: ''
+//   }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]:e.target.value
-    })
-  }
+//   handleChange = (e) => {
+//     this.setState({
+//       [e.target.id]:e.target.value
+//     })
+//   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.signUp(this.state)
-  }
-  render() {
-    const { auth, authError } = this.props;
-    if(auth.uid) return <Redirect to='/'/>
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.props.signUp(this.state)
+//   }
+//   render() {
+//     const { auth, authError } = this.props;
+//     if(auth.uid) return <Redirect to='/'/>
+//     return (
+//       <div className='container'>
+//         <form onSubmit={this.handleSubmit} className='white'>
+//           <h5 className='grey-text text-darken-3'>Sign Up</h5>
+//           <div className='input-field'>
+//             <label htmlFor='email'>Email</label>
+//             <input type='email' id='email' onChange={this.handleChange}/>
+//           </div>
+//           <div className='input-field'>
+//             <label htmlFor='password'>Password</label>
+//             <input type='password' id='password' onChange={this.handleChange}/>
+//           </div>
+//           <div className='input-field'>
+//             <label htmlFor='firstName'>First Name</label>
+//             <input type='text' id='firstName' onChange={this.handleChange}/>
+//           </div>
+//           <div className='input-field'>
+//             <label htmlFor='lastName'>Last Name</label>
+//             <input type='text' id='lastName' onChange={this.handleChange}/>
+//           </div>
+//           <div className='input-field'>
+//             <button className='btn pink lighten-1 z-depth-0'>Sign Up</button>
+//             <div className='red-text center'>
+//               { authError ? <p>{authError}</p> : null}
+//             </div>
+//           </div>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+
+const SignUp = (props) => {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => props.signUp(data)
+  const { auth, authError } = props;
+
+  if(auth.uid) return <Redirect to='/'/>
     return (
       <div className='container'>
-        <form onSubmit={this.handleSubmit} className='white'>
+        <form onSubmit={handleSubmit(onSubmit)} className='white'>
           <h5 className='grey-text text-darken-3'>Sign Up</h5>
           <div className='input-field'>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' onChange={this.handleChange}/>
+            <input name='email' type='email' id='email' ref={register({required: true})}/>
           </div>
           <div className='input-field'>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' onChange={this.handleChange}/>
+            <input type='password' id='password' name="password" ref={register({required: true})}/>
           </div>
           <div className='input-field'>
             <label htmlFor='firstName'>First Name</label>
-            <input type='text' id='firstName' onChange={this.handleChange}/>
+            <input type='text' id='firstName' name="firstName" ref={register({required: true})}/>
           </div>
           <div className='input-field'>
             <label htmlFor='lastName'>Last Name</label>
-            <input type='text' id='lastName' onChange={this.handleChange}/>
+            <input type='text' id='lastName' name="lastName" ref={register({required: true})}/>
           </div>
           <div className='input-field'>
             <button className='btn pink lighten-1 z-depth-0'>Sign Up</button>
@@ -53,7 +93,6 @@ class SignUp extends Component {
         </form>
       </div>
     )
-  }
 }
 
 const mapStateToProps = (state) => {
